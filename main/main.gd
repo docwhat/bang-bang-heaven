f3bd97842ff00c712155e011fc2807c2e7c15694
Christian Höltje <docwhat@gerf.org>
2025-10-28 02:20:34 -0400

    add animtree with attacking

--- main/main.gd
+++ main/main.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 ## The mapping context that we use (GUIDE).
-@export var mapping_context:GUIDEMappingContext
+@export var mapping_context: GUIDEMappingContext
+
 
 ## Setup our game.
 func _ready() -> void:
 	set_window_min_size()
-	GUIDE.enable_mapping_context(mapping_context)	
+	GUIDE.enable_mapping_context(mapping_context)
 
 
 ## Grab the initial size and set the minimum to match it.
--- player/player.gd
+++ player/player.gd
@@ -2,32 +2,36 @@ extends CharacterBody2D
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
-const sprite_size: Vector2 = Vector2(16, 16)
-
 ## The speed of the player.
-@export var speed:float = 100
+@export var speed: float = 100
 
 ## The action that moves the player.
-@export var move_action:GUIDEAction
+@export var move_action: GUIDEAction
 
-var last_direction: Vector2 = Vector2.ZERO
+## The action for attacking.
+@export var attack_action: GUIDEAction
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+var facing: Vector2 = Vector2.ZERO
+var is_attacking: bool = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	velocity = move_action.value_axis_2d
-	
-func _physics_process(delta: float) -> void:
-	var screen_size = get_viewport_rect().size
-	
-	animation_tree.set("parameters/blend_position", velocity.normalized())
-	
-	if velocity.length() > 0:
-		last_direction = velocity.normalized()
-		velocity = last_direction * speed
-		position += velocity * delta
 
-	position = position.clamp(sprite_size / 2, screen_size - sprite_size / 2)
+func _process(_delta: float) -> void:
+	is_attacking = attack_action.is_triggered()
+
+	if move_action.value_axis_2d.length() > 0.0:
+		facing = move_action.value_axis_2d.normalized()
+
+		if is_attacking:
+			velocity = Vector2.ZERO
+		else:
+			velocity = move_action.value_axis_2d * speed
+	else:
+		velocity = Vector2.ZERO
+
+
+func _physics_process(_delta: float) -> void:
+	animation_tree.set("parameters/idle/blend_position", facing)
+	animation_tree.set("parameters/walk/blend_position", facing)
+	animation_tree.set("parameters/attack/blend_position", facing)
+
+	move_and_slide()
