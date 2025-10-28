62a7c9164967a4557cbab6745d628e6480b1517b
Christian Höltje <docwhat@gerf.org>
2025-10-28 00:43:04 -0400

    checkpoint
    Got basic GUIDE movement up.
    Setup some AnimationTree with a 2D blend.

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,8 @@
-extends Area2D
+extends CharacterBody2D
 
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+const sprite_size: Vector2 = Vector2(16, 16)
 
 ## The speed of the player.
 @export var speed:float = 100
@@ -9,10 +10,24 @@ extends Area2D
 ## The action that moves the player.
 @export var move_action:GUIDEAction
 
+var last_direction: Vector2 = Vector2.ZERO
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animated_sprite_2d.play(&"idle_front")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += move_action.value_axis_2d * speed * delta
+	velocity = move_action.value_axis_2d
+	
+func _physics_process(delta: float) -> void:
+	var screen_size = get_viewport_rect().size
+	
+	animation_tree.set("parameters/blend_position", velocity.normalized())
+	
+	if velocity.length() > 0:
+		last_direction = velocity.normalized()
+		velocity = last_direction * speed
+		position += velocity * delta
+
+	position = position.clamp(sprite_size / 2, screen_size - sprite_size / 2)
